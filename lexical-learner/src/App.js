9a90827be18ade0e6fa547d9561073c0ed3d1821
Jacky Chen <tuo17505@temple.edu>
2022-02-24 03:12:51 -0500

    added react-router-dom@6

--- lexical-learner/src/App.js
+++ lexical-learner/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Upload from './pages/Upload';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/upload" element={<Upload />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ lexical-learner/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>HOME PAGE</div>
+  )
+}
+
+export default Home
