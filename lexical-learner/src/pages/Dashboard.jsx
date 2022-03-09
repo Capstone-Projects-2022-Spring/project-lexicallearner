174fc00d779f6b891721d2a19be7a8d51a318141
Shane Grinkewitz <shanegrinkewitz@gmail.com>
2022-03-09 12:11:54 -0500

    Updated Dashboard

--- lexical-learner/src/pages/Dashboard.jsx
+++ lexical-learner/src/pages/Dashboard.jsx
@@ -7,13 +7,13 @@ const Dashboard = () => {
     <div className="Dashboard">
       <Navbar />
       <section className="classrooms">
-        <div className="classroom">
+        <div className="classroom" onclick="location.href='#';">
           Classroom 1
         </div>
-        <div className="classroom">
+        <div className="classroom" onclick="location.href='#';">
           Classroom 2
         </div>
-        <div className="classroom">
+        <div className="classroom" onclick="location.href='#';">
           Classroom 3
         </div>
       </section>
