61084718a6316b7d044bed3b938bebda25de9d84
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-03-13 08:08:18 -0400

    create-db using run-mysql-script

--- database/create-db.js
+++ database/create-db.js
@@ -7,37 +7,19 @@
 /* for reading files */
 const fs = require('fs');
 /* for starting the database */
-const startDb = require('./start-db');
+const runMySqlScript = require('./run-mysql-script');
 
 /* constants */
 /* template for creating database */
 const TEMPLATE_FILE = 'create-db-template.sql';
 
-/* test whether it works */
-startDb((connection, database) => {
-  /* read the template file */
-  fs.readFile(TEMPLATE_FILE, 'utf8', (err, res) => {
-    /* if any errors */
-    if (err) {
-      /* cascade the error */
-      throw err;
-    } /* end if (err) */
+runMySqlScript(TEMPLATE_FILE, (err, res) => {
+  /* if any errors */
+  if (err) {
+    /* cascade the error */
+    throw err;
+  } /* end if (err) */
 
-    /* fill in the template */
-    const SQL_SCRIPT = res.replace(/@\{database\}/g, database);
-    for (const LINE of SQL_SCRIPT.split(';')) {
-      /* log the line */
-      console.log('Executing: ', LINE);
-      connection.query(LINE, [], (err, res) => {
-        /* if any errors */
-        if (err) {
-          /* cascade the error */
-          throw err;
-        } /* end if (err) */
-
-        /* log the response */
-        console.log(res);
-      });
-    } /* next LINE */
-  }); /* end callback fs.readFile */
-}); /* end callback startDb */
+  /* log the response */
+  console.log(res);
+}) /* end callback runMySqlScript */;
