a757ff792705870bec97e084d08db95338549cdd
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-03-13 01:41:54 -0500

    build-db tests importing, running connection

---
+++ database/create-db.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/**
+ * Builds the database.
+ */
+
+/* imports */
+/* test that can import startDb */
+const startDb = require('./start-db');
+
+/* test whether it works */
+startDb();
