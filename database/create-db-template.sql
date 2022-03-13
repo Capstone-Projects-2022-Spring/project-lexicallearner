93859fa7875d8959b5b665b640fba6f19ae0b294
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-03-13 07:14:05 -0400

    question table

--- database/create-db-template.sql
+++ database/create-db-template.sql
@@ -7,13 +7,13 @@ SHOW DATABASES;
 -- Enter the database
 USE @{database};
 
--- Create the Accounts table
+-- Create the Accounts entity table
 CREATE TABLE Account (
   acid      INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   password  VARCHAR(40) NOT NULL
 );
 
--- Create the Profile table
+-- Create the Profile entity table
 CREATE TABLE Profile (
   pfid      INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   acid      INT         NOT NULL                FOREIGN KEY,
@@ -21,16 +21,25 @@ CREATE TABLE Profile (
   score     INT         NOT NULL
 );
 
--- Create the Lesson table
+-- Create the Lesson entity table
 CREATE TABLE Lesson (
   lsid      INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   pfid      INT         NOT NULL                FOREIGN KEY,
   lsLevel   INT         NOT NULL
 );
 
--- Create the Question table
+-- Create the Question entity table
 CREATE TABLE Question (
   qsid      INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   lsid      INT         NOT NULL                FOREIGN KEY,
   qsItid    INT         NOT NULL                FOREIGN KEY
 );
+
+-- Create the answers relation table
+CREATE TABLE Question (
+  anid      INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  qsid      INT         NOT NULL                FOREIGN KEY,
+  itid      INT         NOT NULL                FOREIGN KEY,
+  isCorrect BOOLEAN     NOT NULL  
+);
+
