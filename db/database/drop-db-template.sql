099e63b0fcf132f6d76910839e734f5856091ace
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-03-21 18:10:44 -0400

    table of tables for database

--- db/database/drop-db-template.sql
+++ db/database/drop-db-template.sql
@@ -1,6 +1,7 @@
 -- Template for deleting the database given by `@{database}`.
 
--- Enter the database
+-- Enter the database, creating it if it does not exist.
+CREATE DATABASE IF NOT EXISTS @{database};
 USE @{database};
 
 -- Clean up all tables if they already exist, and check
@@ -10,10 +11,10 @@ DROP TABLE IF EXISTS Item;
 DROP TABLE IF EXISTS Lesson;
 DROP TABLE IF EXISTS Profile;
 DROP TABLE IF EXISTS Account;
-DROP TABLE IF EXISTS Room;
 DROP TABLE IF EXISTS Message;
+DROP TABLE IF EXISTS Room;
+DROP TABLE IF EXISTS Tables_in;
 SHOW TABLES;
-
 
 -- Now remove the database, and check
 DROP DATABASE @{database};
