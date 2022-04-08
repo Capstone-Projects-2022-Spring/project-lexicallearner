563c70beb335a6f1507e56c234c0bb66eb2b5e35
Shane Grinkewitz <shanegrinkewitz@gmail.com>
2022-04-07 20:35:47 -0400

    Finished Image Upload Backend

--- server/server.js
+++ server/server.js
@@ -1,21 +1,28 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const formidable = require('formidable');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
 app.post('/image_upload', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+  var filename = "";
+  var form = new formidable.IncomingForm();
+
+  form.parse(req, function (err, fields, files) {
+    console.log(files);
+    if (files.filename.mimetype.startsWith('image/')) {
+      console.log('File uploaded, path: ' + files.filename.filepath);
+      var oldpath = files.filename.filepath;
+      var newpath = './uploaded_images/' + files.filename.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+      });
+    } else {
+      console.log("File is not an image");
+    }
+    res.redirect("/upload");
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
