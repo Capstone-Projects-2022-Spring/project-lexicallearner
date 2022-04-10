5fbde5f490f87b909b8928554dcc1ab3177c0689
Grant Gwiaz <tug66039@temple.edu>
2022-04-10 01:26:38 -0400

    Add express api for translation and remove clutter

--- server/server.js
+++ server/server.js
@@ -3,107 +3,78 @@ const formidable = require('formidable');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
-
-app.post('/image_upload', (req, res) => {
-  var filename = "";
-  var form = new formidable.IncomingForm();
-
-  form.parse(req, function (err, fields, files) {
-    console.log(files);
-    if (files.filename.mimetype.startsWith('image/')) {
-      console.log('File uploaded, path: ' + files.filename.filepath);
-      var oldpath = files.filename.filepath;
-      var newpath = './uploaded_images/' + files.filename.originalFilename;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-      });
-    } else {
-      console.log("File is not an image");
-    }
-    res.redirect("/upload");
-  });
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+app.post('/image_upload', (req, res) => {
+    var filename = "";
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        console.log(files);
+        if (files.filename.mimetype.startsWith('image/')) {
+            console.log('File uploaded, path: ' + files.filename.filepath);
+            var oldpath = files.filename.filepath;
+            var newpath = './uploaded_images/' + files.filename.originalFilename;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+            });
+        } else {
+            console.log("File is not an image");
+        }
+        res.redirect("/upload");
+    });
+});
 
 
 
 
-
-
-
-
-app.use(express.json());
-
-
+//detectText API call like this: https://.../api/translate/?file=...    You can put a local file or a image url
 app.get('/api/detectText', async (req, res) => {
-  const text = await detectAndTranslateImg();
-  res.send(text);
+    const fileName = req.query.file;
+    const text = await detectTextAndLangImg(fileName);
+    res.send(text);
 });
 
-app.get('/api/books/:file', (req, res) => {
-  const book = books.find(c => c.id === parseInt(req.params.id));
-
-  if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>');
-  res.send(book);
-});
-
-//CREATE Request Handler
-app.post('/api/books', (req, res)=> {
-
-  const { error } = validateBook(req.body);
-  if (error){
-    res.status(400).send(error.details[0].message)
-    return;
-  }
-  const book = {
-    id: books.length + 1,
-    title: req.body.title
-  };
-  books.push(book);
-  res.send(book);
-});
-
-//UPDATE Request Handler
-app.put('/api/books/:id', (req, res) => {
-  const book = books.find(c=> c.id === parseInt(req.params.id));
-  if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Not Found!! </h2>');
-
-  const { error } = validateBook(req.body);
-  if (error){
-    res.status(400).send(error.details[0].message);
-    return;
-  }
-
-  book.title = req.body.title;
-  res.send(book);
-});
-
-//DELETE Request Handler
-app.delete('/api/books/:id', (req, res) => {
-
-  const book = books.find( c=> c.id === parseInt(req.params.id));
-  if(!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;"> Not Found!! </h2>');
-
-  const index = books.indexOf(book);
-  books.splice(index,1);
-
-  res.send(book);
-});
-
-
-async function detectAndTranslateImg(fileNAme) {
-  const vision = require('@google-cloud/vision');
+// TODO extract text from the JSON a JSON that contains the original text "locale" (ie detected language) and translated text (add target lang to params)
+async function detectTextAndLangImg(fileName) {
+    try {
+        const vision = require('@google-cloud/vision');
 
 // Creates a client
-  const client = new vision.ImageAnnotatorClient();
-  const fileName = '.\\wakeupcat.jpg';
+        const client = new vision.ImageAnnotatorClient();
 
 // Performs text detection on the local file
-  const [result] = await client.textDetection(fileName);
-  const detections = result.textAnnotations;
-  detections.forEach(text => console.log(text));
-  return detections;
+        const [result] = await client.textDetection(fileName);
+        const detections = result.textAnnotations;
+        detections.forEach(text => console.log(text));
+        return detections;
+    } catch (e) {
+        return e.toString();
+    }
+}
+
+// Translate API call like this: https://.../api/translate/?text=...&targetLang=...     put in the text you anna translate and the language code that your translating to
+app.get('/api/translate', async (req, res) => {
+    const oriText = req.query.text;
+    const lang = req.query.targetLang;
+    const text = await translateText(oriText, lang);
+    res.send(text);
+});
+
+async function translateText(oriText, targetLang) {
+    try {
+        // Imports the Google Cloud client library
+        const {Translate} = require('@google-cloud/translate').v2;
+        // Instantiates a client
+        const translate = new Translate();
+
+        const [translation] = await translate.translate(oriText, targetLang);
+        //returns a string of the translate text not json
+        return translation;
+    } catch (e) {
+        return e.toString();
+    }
 }
