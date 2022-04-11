e27728a9a9a42fcf0b3b707e1211cfeb74cebb25
Grant Gwiaz <tug66039@temple.edu>
2022-04-10 23:01:56 -0400

    Prevent an error in upload image api

--- server/server.js
+++ server/server.js
@@ -8,24 +8,29 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.post('/image_upload', (req, res) => {
-    var filename = "";
-    var form = new formidable.IncomingForm();
+app.post('/api/image_upload', (req, res) => {
+    try {
+        let filename = "";
+        const form = new formidable.IncomingForm();
 
-    form.parse(req, function (err, fields, files) {
-        console.log(files);
-        if (files.filename.mimetype.startsWith('image/')) {
-            console.log('File uploaded, path: ' + files.filename.filepath);
-            var oldpath = files.filename.filepath;
-            var newpath = './uploaded_images/' + files.filename.originalFilename;
-            fs.rename(oldpath, newpath, function (err) {
-                if (err) throw err;
-            });
-        } else {
-            console.log("File is not an image");
-        }
-        res.redirect("/upload");
-    });
+        form.parse(req, function (err, fields, files) {
+            console.log(files);
+            if (files.filename.mimetype.startsWith('image/')) {
+                console.log('File uploaded, path: ' + files.filename.filepath);
+                let oldpath = files.filename.filepath;
+
+                let newpath = './uploaded_images/' + files.filename.originalFilename;
+                fs.rename(oldpath, newpath, function (err) {
+                    if (err) throw err;
+                });
+            } else {
+                console.log("File is not an image");
+            }
+            res.redirect("/upload");
+        });
+    } catch (e) {
+        console.log(e.toString());
+    }
 });
 
 
@@ -44,19 +49,21 @@ async function detectTextAndLangImg(fileName) {
         const vision = require('@google-cloud/vision');
 
 // Creates a client
+        //make sure that the API JSON file is in the project and set
         const client = new vision.ImageAnnotatorClient();
 
 // Performs text detection on the local file
         const [result] = await client.textDetection(fileName);
         const detections = result.textAnnotations;
         detections.forEach(text => console.log(text));
+
         return detections;
     } catch (e) {
         return e.toString();
     }
 }
 
-// Translate API call like this: https://.../api/translate/?text=...&targetLang=...     put in the text you anna translate and the language code that your translating to
+// Translate API call like this: https://.../api/translate/?text=...&targetLang=...     put in the text you want to translate and the language code that your translating to
 app.get('/api/translate', async (req, res) => {
     const oriText = req.query.text;
     const lang = req.query.targetLang;
