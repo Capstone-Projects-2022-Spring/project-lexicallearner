929d216ac1d31b70bdb8b674b532d34d41717296
Jacky Chen <tuo17505@temple.edu>
2022-04-16 22:42:33 -0400

    mib

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -13,7 +13,7 @@ import ProfileModal from "../chat-profilemodal/ProfileModal";
 //connect to chat server
 const socket = io(process.env.REACT_APP_CHAT_SERVER_URL || process.env.CHAT_SERVER_URL);
 const Chat = (props) => {
-  console.log(process.env.CHAT_SERVER_URL);
+  console.log(process.env.REACT_APP_CHAT_SERVER_URL);
   let pref_lang = localStorage.getItem("preferred_language");
   if (!pref_lang) pref_lang = "es";
   const [preferredLanguage, setPreferredLanguage] = useState(pref_lang);
